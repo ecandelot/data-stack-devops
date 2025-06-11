f21330572f9763b2e2ba00e2a708291380a87a72
ecandelot <etienne.candelot.pro@gmail.com>
2025-06-11 17:57:00 +0200

    le charbon

--- docker/fluent-bit/entrypoint.sh
+++ docker/fluent-bit/entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Using Loki port: $LOKI_PORT"
-envsubst < /fluent-bit/fluent-bit.conf.template > /fluent-bit/fluent-bit.conf
+envsubst < /docker/fluent-bit/fluent-bit.conf.template > /docker/fluent-bit/fluent-bit.conf
 
 # Pour le débogage, affichez le contenu du fichier généré
 echo "Generated Fluent-bit configuration:"
-cat /fluent-bit/fluent-bit.conf
+cat /docker/fluent-bit/fluent-bit.conf
 
 # Exécutez Fluent-bit avec le fichier de configuration généré
-exec /fluent-bit/bin/fluent-bit -c /fluent-bit/fluent-bit.conf
+exec /fluent-bit/bin/fluent-bit -c /docker/fluent-bit/fluent-bit.conf
