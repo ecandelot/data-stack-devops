da482f2309d02ffd3730bb4079f521a66b922ab4
ecandelot <etienne.candelot.pro@gmail.com>
2025-06-11 15:30:10 +0200

    make entrypoint executable

--- docker/fluent-bit/entrypoint.sh
+++ docker/fluent-bit/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+set -e
+echo "Using Loki port: $LOKI_PORT"
 envsubst < /fluent-bit/etc/fluent-bit.conf.template > /fluent-bit/etc/fluent-bit.conf
+cat /fluent-bit/etc/fluent-bit.conf  # pour debug
 exec /fluent-bit/bin/fluent-bit -c /fluent-bit/etc/fluent-bit.conf
